7529ebe0d2eb466008bdff93646d506460a3a686
Peter (pir) Radcliffe <pir-github-turning@pir.net>
2019-11-09 12:52:55 +0000

    Add ESP32 specific restart utility

--- arduino/turning/utils.h
+++ arduino/turning/utils.h
@@ -8,4 +8,11 @@
 #define STR_IMPL_(x) #x
 #define STR(x) STR_IMPL_(x)
 
+// ESP specific
+#ifdef ESP32
+#include <esp_int_wdt.h>
+#include <esp_task_wdt.h>
+void do_restart();
+#endif //ESP32
+
 #endif //TURNING_UTILS_H
---
+++ arduino/turning/utils.ino
@@ -0,0 +1,11 @@
+/*
+ * Utilities
+ */
+
+#ifdef ESP32
+void do_restart() {
+  esp_task_wdt_init(1,true);
+  esp_task_wdt_add(NULL);
+  while(true);
+}
+#endif //ESP32
